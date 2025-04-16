cee65042843ebb731684c74ae8b99cfd258355c5
Prabal Bhandary <prabalbhandary97@gmail.com>
2025-04-16 11:39:32 +0545

    Refactor server index.js to improve file path handling and streamline production setup

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 import fileUpload from "express-fileupload";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import path from "path";
 
 import connectDB from "./src/config/db.js";
@@ -22,13 +24,15 @@ import { removeUnverifiedAccount } from "./src/services/removeUnverifiedAccount.
 const app = express();
 const port = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
 const corsOptions = {
   origin: [process.env.FRONTEND_URL],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
+
+app.get("/test", (req, res) => {
+  res.send("Test route working");
+});
 
 app.use(cors(corsOptions));
 app.use(morgan("dev"));
@@ -50,17 +54,23 @@ app.use("/api/v1/book", bookRoutes);
 app.use("/api/v1/borrow", borrowRoutes);
 app.use("/api/v1/user", userRoutes);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/dist")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+  });
+}
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-
-app.use(express.static(path.join(__dirname, "./client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/dist/index.html"));
-});
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   connectDB();
@@ -68,5 +78,3 @@ app.listen(port, () => {
   removeUnverifiedAccount();
   console.log(`Server is running on http://localhost:${port}`.bgMagenta.white);
 });
-
-app.use(errorMiddleware);
